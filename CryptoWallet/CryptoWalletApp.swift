d1303f632a9b0480c30e638b548a24b63fe7e3ba
Ricardo Silva <ricardo@ricardocsiva.com>
2022-05-09 18:58:33 +0100

    Initial Project Setup

---
+++ CryptoWallet/CryptoWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoWalletApp.swift
+//  CryptoWallet
+//
+//  Created by ricardo silva on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
