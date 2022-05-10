bf16aaf1af2c5a90b5328adc9eadc728457996f9
Ricardo Silva <ricardo@ricardocsiva.com>
2022-05-10 15:14:25 +0100

    Create Coin list with dummy data

---
+++ CryptoWallet/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewModel.swift
+//  CryptoWallet
+//
+//  Created by ricardo silva on 10/05/2022.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    
+    @Published var allCoins: [Coin] = []
+    @Published var portfolioCoins: [Coin] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+}
